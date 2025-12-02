475d2b0b0a7c9948164e52092def3b1abf4c83a2
shandiru <msddiru@gmail.com>
2025-12-02 19:45:15 +0530

    added

--- src/Page/BuyDetails.jsx
+++ src/Page/BuyDetails.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import GallerySection from '../components/BuyDetails/Gallery'
 import UnderTheBonnet from '../components/BuyDetails/UnderTheBonnet'
-
+import VehicleDetailsSection from "../components/BuyDetails/VehicleDetailsSection"
 const BuyDetails = () => {
     return (
         <div>
+            <VehicleDetailsSection />
             <UnderTheBonnet />
             <GallerySection />
         </div>
